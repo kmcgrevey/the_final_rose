6b2ec1957fa624a4bf5b606b040ec46f2edeae01
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-03 18:00:29 -0700

    Adds Contestant Outings test

--- spec/features/contestants_show_spec.rb
+++ spec/features/contestants_show_spec.rb
@@ -29,6 +29,40 @@ RSpec.describe "When I visit contestant show page", type: :feature do
     click_on @cont1.name
     expect(current_path).to eq("/contestants/#{@cont1.id}")
   end
+
+  it "I see contestant's season and outings info" do
+    out1 = Outings.create!(name: "Kickball",
+                           location: "Detroit",
+                           date: "01/02/20")
+    out2 = Outings.create!(name: "Helicopter Ride",
+                           location: "Bali",
+                           date: "09/12/19")
+    click_on @cont1.name
+
+    expect(page).to have_content(@cont1.name)
+    expect(page).to have_content(15)
+    expect(page).to have_content("The Most Dramatic Season Yet!")
+    expect(page).to have_content(out1.name)
+    expect(page).to have_content(out2.name)
+
+    click_on out1.name
+  end
  
 end
+
+# As a visitor,
+# When I visit a contestants show page,
+# I see that contestants name as well as the
+# season number and season description that this contestant was on.
+# I also see a list of names of the outings that this contestant
+#has been on while on the show.
+# (e.g.
+#                         Ben Higgins
+# Season 20 - No wait, THIS is the most dramatic season yet
+
+# Outings: Kickball
+#         Hot Springs
+#         Helicopter Ride
+# )
+# When I click on an outing name, I'm taken to that outings show page
   
