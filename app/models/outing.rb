5b108d1d02955b6f2098a36653dd0a0f82c505a3
Kevin McGrevey <niv3kmcg@gmail.com>
2020-11-09 18:05:38 -0700

    Adds instance methods and tests to Outing

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -1,4 +1,14 @@
 class Outing < ApplicationRecord
   has_many :contestant_outings
   has_many :contestants, through: :contestant_outings
+
+  def names_list
+    list = contestants +
+          (contestants.map { |cont| cont.bachelorette }).uniq
+    list.map { |person| person.name }
+  end
+
+  def body_count
+    names_list.count
+  end
 end
--- spec/models/outing_spec.rb
+++ spec/models/outing_spec.rb
@@ -5,4 +5,40 @@ RSpec.describe Outing, type: :model do
     it {should have_many :contestant_outings}
     it {should have_many(:contestants).through(:contestant_outings)}
   end
+
+  describe 'methods' do
+    before(:each) do
+      @bach1 = Bachelorette.create!(name: "Hannah Brown",
+                                 season_number: 15,
+                                 season_desc: "The Most Dramatic Season Yet!")
+      @cont1 = Contestant.create!(name: "Pilot Pete",
+                                  age: 34,
+                                  hometown: "Irving, TX",
+                                  bachelorette_id: @bach1.id)
+      @cont2 = Contestant.create!(name: "Diver Dan",
+                                  age: 43,
+                                  hometown: "Destin, FL",
+                                  bachelorette_id: @bach1.id)
+      @out1 = Outing.create!(name: "Kickball",
+                            location: "Detroit",
+                            date: "01/02/20")
+      @out2 = Outing.create!(name: "Helicopter Ride",
+                            location: "Bali",
+                            date: "09/12/19")
+      ContestantOuting.create!(contestant_id: @cont1.id,
+                              outing_id: @out2.id)
+      ContestantOuting.create!(contestant_id: @cont2.id,
+                              outing_id: @out2.id)
+    end
+    
+    it 'names_list' do
+      expect(@out2.names_list).to include("Pilot Pete",
+                                          "Diver Dan",
+                                          "Hannah Brown")
+    end
+
+    it 'body_count' do
+      expect(@out2.body_count).to eq(3)
+    end
+  end
 end
